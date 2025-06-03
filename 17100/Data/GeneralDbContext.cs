81db89e9e6561ad2aa65e95300e26064e3bd9355
00017100wiut <00017100wiut@gmail.com>
2025-06-03 11:45:34 +0500

    Add GeneralDbContext with Product DbSet

---
+++ 17100/Data/GeneralDbContext.cs
@@ -0,0 +1,15 @@
+﻿using _17100.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace _17100.Data
+{
+    public class GeneralDbContext : DbContext
+    {
+        public GeneralDbContext(DbContextOptions<GeneralDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; } = null!;
+    }
+}
