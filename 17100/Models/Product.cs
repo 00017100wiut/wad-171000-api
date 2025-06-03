8d613be1d021aae97eeec2839662e6a39a2507d2
00017100wiut <00017100wiut@gmail.com>
2025-06-03 11:45:29 +0500

    Add Product model

---
+++ 17100/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _17100.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        [MaxLength(100)]
+        public string Name { get; set; } = null!;
+
+        [Required(ErrorMessage = "Price is required")]
+        [Range(0, double.MaxValue, ErrorMessage = "Price must be non-negative")]
+        public decimal Price { get; set; }
+
+        [MaxLength(500)]
+        public string? Details { get; set; }
+
+        [Required(ErrorMessage = "SKU is required")]
+        public int SKU { get; set; }
+    }
+}
